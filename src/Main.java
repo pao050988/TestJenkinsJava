387c732573eaa030ab215dc4826e6b1595fb3816
Paola <pao050988@hotmail.com}>
2023-10-03 12:11:14 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.print("Fucking Jenkins ");
+		System.out.println("Build Number:"+System.getenv("BUILD_NUMBER")); //BUILD_NUMBER - name of the environment variable;
 
 
 	}
